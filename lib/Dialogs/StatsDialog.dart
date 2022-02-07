22bd088eef181831754bcf7e0b9fe8495e2ecda1
Andrew Zuo <andrew@andrewzuo.com>
2022-02-06 21:54:59 -0500

    Tweaked debrief screen

--- lib/Dialogs/StatsDialog.dart
+++ lib/Dialogs/StatsDialog.dart
@@ -21,13 +21,6 @@ class StatsDialogState extends State<StatsDialog> {
 			title: Center(
 				child: Text(puzzle.solved ? "Congratulations!" : "Statistics"),
 			),
-			content: Text(
-					"Wins: $numWins\n"
-					"Played: $numPlays\n"
-					"Win percentage: $winPercentage%\n"
-					"Current Streak: $currentStreak\n"
-					"Max Streak: $maxStreak\n"
-			),
 			actions: <Widget>[
 				TextButton(
 					child: Text("SHUFFLE", style: Theme.of(context).textTheme.bodyText1),
@@ -39,13 +32,6 @@ class StatsDialogState extends State<StatsDialog> {
 						Navigator.pop(context);
 					},
 				),
-				puzzle.solved ? TextButton(
-					child: Text("SHARE (CLIPBOARD)", style: Theme.of(context).textTheme.bodyText1),
-					onPressed: () {
-						Clipboard.setData(ClipboardData(text: "I solved the Flurtle for ${todaysDate(DateTime.now())} in ${puzzle.numChecks} checks and ${puzzle.numMoves} moves.${puzzle.shareInfo}"));
-						Fluttertoast.showToast(msg: "Copied to clipboard");
-					},
-				) : Container(),
 				TextButton(
 					child: Text("DISMISS", style: Theme.of(context).textTheme.bodyText1),
 					onPressed: () => Navigator.pop(context),
